0ff5dc9656d00b1ad64fa6d3a6a074cd79e32e29
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2018-02-07 22:13:14 +0000

    Create IoQueue type

---
+++ src/io_queue.rs
@@ -0,0 +1,56 @@
+use std::collections::VecDeque;
+use std::io;
+use std::io::{Read, Write};
+
+use futures::{Async, Poll};
+use tokio_io::{AsyncRead, AsyncWrite};
+
+pub struct IoQueue {
+    buffer: VecDeque<u8>,
+}
+
+impl IoQueue {
+    pub fn new() -> Self {
+        IoQueue {
+            buffer: VecDeque::new(),
+        }
+    }
+
+    pub fn with_capacity(capacity: usize) -> Self {
+        IoQueue {
+            buffer: VecDeque::with_capacity(capacity),
+        }
+    }
+}
+
+impl Read for IoQueue {
+    fn read(&mut self, buffer: &mut [u8]) -> io::Result<usize> {
+        let count = buffer.len().min(self.buffer.len());
+
+        for (byte, index) in self.buffer.drain(0..count).zip(0..count) {
+            buffer[index] = byte;
+        }
+
+        Ok(count)
+    }
+}
+
+impl AsyncRead for IoQueue {}
+
+impl Write for IoQueue {
+    fn write(&mut self, bytes: &[u8]) -> io::Result<usize> {
+        self.buffer.extend(bytes);
+
+        Ok(bytes.len())
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        Ok(())
+    }
+}
+
+impl AsyncWrite for IoQueue {
+    fn shutdown(&mut self) -> Poll<(), io::Error> {
+        Ok(Async::Ready(()))
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,13 @@ extern crate tokio_proto;
 mod intercept_io;
 mod io_channel;
 mod io_interceptor;
+mod io_queue;
 mod next_item;
 mod possibly_intercepted_io;
 mod protocol_interceptor;
 
 pub use io_channel::IoChannel;
 pub use io_interceptor::IoInterceptor;
+pub use io_queue::IoQueue;
 pub use next_item::NextItem;
 pub use protocol_interceptor::ProtocolInterceptor;
