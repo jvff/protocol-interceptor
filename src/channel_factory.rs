cba8953f994669d797b7b1a5896b47470f277f15
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2018-02-08 20:14:48 +0000

    Wait for read channels to be created
    - Only create them when write channels are requested

--- src/channel_factory.rs
+++ src/channel_factory.rs
@@ -1,5 +1,8 @@
 use std::collections::VecDeque;
 
+use futures::Async;
+use futures::task;
+use futures::task::Task;
 use futures::sync::BiLock;
 use tokio_io::AsyncRead;
 use tokio_io::io::{ReadHalf, WriteHalf};
@@ -12,6 +15,7 @@ pub(crate) type ReadChannel = (ReadHalf<IoQueue>, ReadHalf<IoQueue>);
 pub(crate) type WriteChannel = (WriteHalf<IoQueue>, WriteHalf<IoQueue>);
 
 pub struct ChannelFactory {
+    read_task: Option<Task>,
     read_channels: VecDeque<ReadChannel>,
     write_channels: VecDeque<WriteChannel>,
 }
@@ -19,6 +23,7 @@ pub struct ChannelFactory {
 impl ChannelFactory {
     pub fn new() -> Self {
         ChannelFactory {
+            read_task: None,
             read_channels: VecDeque::new(),
             write_channels: VecDeque::new(),
         }
@@ -30,17 +35,20 @@ impl ChannelFactory {
         (ReadChannels::new(read_lock), WriteChannels::new(write_lock))
     }
 
-    pub(crate) fn read_channel(&mut self) -> ReadChannel {
-        self.read_channels.pop_front().unwrap_or_else(|| {
-            let (read_channel, write_channel) = self.new_channel();
+    pub(crate) fn read_channel(&mut self) -> Async<ReadChannel> {
+        if let Some(read_channel) = self.read_channels.pop_front() {
+            Async::Ready(read_channel)
+        } else {
+            self.park_read_task();
 
-            self.write_channels.push_back(write_channel);
-
-            read_channel
-        })
+            Async::NotReady
+        }
     }
 
+
     pub(crate) fn write_channel(&mut self) -> WriteChannel {
+        self.notify_read_task();
+
         self.write_channels.pop_front().unwrap_or_else(|| {
             let (read_channel, write_channel) = self.new_channel();
 
@@ -61,5 +69,24 @@ impl ChannelFactory {
         let write_channel = (write_request, write_response);
 
         (read_channel, write_channel)
+    }
+
+    fn notify_read_task(&mut self) {
+        if let Some(task) = self.read_task.take() {
+            task.notify();
+        }
+    }
+
+    fn park_read_task(&mut self) {
+        if self.read_task.is_some() {
+            panic!(
+                concat!(
+                    "two WriteChannels are attempting to use the same ",
+                    "ChannelFactory",
+                )
+            )
+        }
+
+        self.read_task = Some(task::current());
     }
 }
--- src/read_channels.rs
+++ src/read_channels.rs
@@ -22,7 +22,7 @@ impl Stream for ReadChannels {
     fn poll(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
         let lock_result = Ok(self.channel_factory.poll_lock());
         let mut channel_factory = try_ready!(lock_result);
-        let read_channel = channel_factory.read_channel();
+        let read_channel = try_ready!(Ok(channel_factory.read_channel()));
 
         Ok(Async::Ready(Some(read_channel)))
     }
